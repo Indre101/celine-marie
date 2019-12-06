617309c73db4cc945e6c75c25fbce519620888f6
indre101 <zygaityte.indre@gmail.com>
2019-12-06 19:09:03 +0100

    adds image to folder

--- main.js
+++ main.js
@@ -10,11 +10,31 @@ fetch("http://indre101.lashboutique.dk/wordpress/wp-json/wp/v2/art_categories").
 })
 
 
+// <img class="image-sample" src="./images/water.png" alt="">
+
 
 
 function getCategories(category) {
   let clnMenuFolder = menuFolderTemplate.cloneNode(true);
   clnMenuFolder.querySelector(".category-name").textContent = category.title.rendered;
+  let imagesInsideFolderIcon = clnMenuFolder.querySelector(".images-inside-folder-icon");
+
+
+  if (category.art_category_id.length > 0) {
+    addImgaesToFolderIcon(category, imagesInsideFolderIcon)
+  }
+
   artCategories.appendChild(clnMenuFolder);
   console.log(category);
+}
+
+
+// Function to customise the folder icon
+function addImgaesToFolderIcon(imageURL, containerToAppendTo) {
+  for (let index = 0; index < 1; index++) {
+    let imageOfArt = document.createElement("img");
+    imageOfArt.classList.add("image-sample");
+    imageOfArt.src = imageURL.art_category_id[index].featured_image.guid
+    containerToAppendTo.appendChild(imageOfArt);
+  }
 }
