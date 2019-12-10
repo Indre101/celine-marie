b12ad76029895be70b3ad99db5bf7d6900b8d5bf
indre101 <zygaityte.indre@gmail.com>
2019-12-10 14:53:33 +0100

    folder structure

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function init() {
   fetch("http://indre101.lashboutique.dk/wordpress/wp-json/wp/v2/art_categories?_embed").then(res => {
     return res.json()
   }).then(data => {
-    appendFolders()
+    appendFolders() //Function that will create and append one main
 
     // console.log(document.querySelector(".open-folder-container"))
     spinner.setAttribute('hidden', ''); //preloader hides
@@ -32,42 +32,105 @@ function init() {
     // appendMyComputerFolder(data) //Function that will append and create the Mycomputer open folder
 
 
-    //   const thisPcBTns = document.querySelectorAll(".this-pc"); //All the this pc buttons in the folder path
-    //   thisPcBTns.forEach(btn => {
-    //     openTheRightFolder(btn, btn.textContent.toLowerCase().split(' ').join('')) //Calls a function that will find the open folder with the name thispc and will display it
-    //   })
+    const thisPcBTns = document.querySelector(".this-pc"); //All the this pc buttons in the folder path
+    // console.log(thisPcBTns);
+    // thisPcBTns.forEach(btn => {
+    // openTheRightFolder(thisPcBTns) //Calls a function that will find the open folder with the name thispc and will display it
+    // })
   }).then(() => {
-    const subcategories = document.querySelectorAll(".subcategory")
+
+    const categoryFolders = document.querySelectorAll(".category-folder")
+    const subcategories = document.querySelectorAll("article.subcategory")
     const childernElements = document.querySelectorAll(".artPieces")
+    const openFolderContainer = document.querySelector(".open-folder-container")
+
+    console.log(subcategories);
+    categoryFolders.forEach(sub => {
+      sub.classList.remove("d-none")
+      sub.onclick = function () {
+        showThisFolderMAin(sub, childernElements)
+        openFolderContainer.classList.remove("d-none")
+        showThisFolderMAin(sub, subcategories)
+      }
+    })
+
     subcategories.forEach(sub => {
-      sub.classList.add("d-none")
+      sub.classList.remove("d-none")
       childernElements.forEach(child => {
         if (child.classList.contains(sub.querySelector(".name").textContent)) {
+          console.log(child);
+          console.log(sub.querySelector(".name"));
+
           sub.appendChild(child)
+          // console.log(child, sub);
+        }
+        sub.onclick = function () {
+          // child.classList.add("d-none");
+          showThisFolderMAin(sub, childernElements)
+          // showThisFolder(sub, childernElements)
+          openFolderContainer.classList.remove("d-none")
         }
       })
-      sub.onclick = function () {
-        showThisFolder(sub, subcategories)
-      }
+
     })
 
   })
 
 }
 
-function showThisFolder(sub, subcategories) {
 
-  for (let index = 0; index < subcategories.length; index++) {
-    console.log(subcategories[index]);
-    subcategories[index].classList.add("d-none");
+function showThisFolderMAin(sub, childernElements) {
+  console.log(sub.querySelector(".name").textContent);
+  childernElements.forEach(child => {
+    child.classList.add("d-none");
 
-  }
+    if (child.classList.contains(sub.querySelector(".name").textContent)) {
+      child.classList.remove("d-none");
+      const subcategoriesOfChild = child.querySelectorAll(".subcategory");
+      if (subcategoriesOfChild.length > 0) {
+        console.log(subcategoriesOfChild);
+        subcategoriesOfChild.forEach(subFolder => {
+          console.log(subFolder);
+          subFolder.classList.remove("d-none");
+        })
+      }
+      let childParent = child.parentElement
+      let childParentParent = childParent.parentElement
+      childParent.classList.remove("d-none");
+      childParentParent.classList.remove("d-none");
+      child.classList.remove("d-none");
+      const icon = childParent.querySelector(".subcategory-icon-and-name");
+      icon.classList.add("d-none");
+    }
+  });
+}
+
+
+function showThisFolder(sub, childernElements) {
+  // for (let index = 0; index < subcategories.length; index++) {
+  //   console.log(subcategories[index]);
+  //   subcategories[index].classList.add("d-none");
+
+  // }
+  childernElements.forEach(child => {
+    if (child.classList.contains(sub.querySelector(".name").textContent)) {
+      // sub.appendChild(child)
+      // sub.classList.add("d-none")
+      // child.classList.remove("d-none");
+      // sub.querySelector(".subcategory-icon-and-name").classList.add("d-none");
+      // sub.querySelector(".artPieces").classList.remove("d-none");
+      // sub.parentElement.classList.remove("d-none")
+      // console.log(child);
+    }
+
+  })
+
   // sub.classList.add("d-none")
-  sub.classList.remove("d-none");
-  sub.querySelector(".subcategory-icon-and-name").classList.add("d-none");
-  sub.querySelector(".artPieces").classList.remove("d-none");
-  sub.parentElement.classList.remove("d-none")
-  console.log(sub);
+  // sub.classList.remove("d-none");
+  // sub.querySelector(".subcategory-icon-and-name").classList.add("d-none");
+  // sub.querySelector(".artPieces").classList.remove("d-none");
+  // sub.parentElement.classList.remove("d-none")
+  // console.log(sub);
 
 }
 
@@ -90,6 +153,7 @@ function appendFolders(category, artArray) {
   parentFolder.classList.add("thisPc")
   openFolderContainers.appendChild(parentFolder)
 
+
   closeButton.onclick = function () { //closes the folder
     // zIndex = 0
 
@@ -99,11 +163,12 @@ function appendFolders(category, artArray) {
 }
 
 
+
 function appendArtPiecesOrSubcategories(category, artArray) {
   const parentFolder = document.querySelector(".thisPc")
-
-
-  // parentFolder.appendChild(parentFolder);
+  const artPieces = document.createElement("div");
+  artPieces.classList.add("subcategory")
+  // artPieces.classList.add("d-none")
 
   let newClassName;
   if (category.post_title) {
@@ -111,6 +176,10 @@ function appendArtPiecesOrSubcategories(category, artArray) {
   } else if (category.title.rendered) {
     newClassName = category.title.rendered.toLowerCase();
   }
+  artPieces.classList.add(newClassName);
+  showSubCategories(category, artPieces)
+
+
   //Checks if the category has post_title OR category.title.rendered and assigns the name of the folder
 
 
@@ -119,36 +188,55 @@ function appendArtPiecesOrSubcategories(category, artArray) {
     const artPieces = document.createElement("div");
     artPieces.classList.add("artPieces")
     // artPieces.classList.add("d-none")
-
     artPieces.classList.add(newClassName);
     parentFolder.appendChild(artPieces);
+
     category.art_category_id.forEach(art => {
-      showArtPieceList(art, artPieces)
-    })
-  }
-  if (category.art_piece_id) {
-    const artPieces = document.createElement("div");
-    artPieces.classList.add("artPieces")
-    // artPieces.classList.add("d-none")
-
-    artPieces.classList.add(newClassName);
-    parentFolder.appendChild(artPieces);
-    artArray.forEach(art => {
       showArtPieceList(art, artPieces)
     })
   }
 
   if (category.subcategory_id) { //Checks if category has subcategories and then calls a function that would display subcategories
-    const artPieces = document.createElement("div");
-    artPieces.classList.add("artPieces")
-    // artPieces.classList.add("d-none")
-
-    artPieces.classList.add(category.title.rendered.toLowerCase());
-    parentFolder.appendChild(artPieces);
+    // const artPieces = document.createElement("div");
+    // artPieces.classList.add("subcategory")
+    // // artPieces.classList.add("d-none")
+    // artPieces.classList.add(newClassName);
+    // parentFolder.appendChild(artPieces);
     category.subcategory_id.forEach(subCategory => {
+      const subcategoryRt = document.createElement("div");
+      subcategoryRt.classList.add("artPieces")
+      subcategoryRt.classList.add(subCategory.post_title.toLowerCase())
+      // artPieces.classList.add("d-none")
+      // subcategory.classList.add(subCategory.post_title.toLowerCase());
+      // console.log(subCategory.post_title.toLowerCase());
+      artPieces.appendChild(subcategoryRt);
+      // console.log(subcategory)
+      // console.log(category)
+      // // if (category.art_piece_id) {
+      //   console.log("pieces id");
+      //   const artPieces = document.createElement("div");
+      //   artPieces.classList.add("artPieces")
+      //   // artPieces.classList.add("d-none")
+      //   artPieces.classList.add(newClassName);
+      //   parentFolder.appendChild(artPieces);
+      //   console.log(parentFolder);
+
+      // const convertedArtArray = Object.keys(subCategory.art_piece_id).map(i => subCategory.art_piece_id[i])
+      // // appendArtPiecesOrSubcategories(subCategory, convertedArtArray)
+
+      // convertedArtArray.forEach(art => {
+      //   console.log(subcategoryRt);
+      //   showArtPieceList(art, subcategoryRt)
+      // })
+      // }
+
+
       showSubCategories(subCategory, artPieces)
+      // artPieces.appendChild(subcategoryRt)
+
     })
   }
+  parentFolder.appendChild(artPieces);
 }
 
 
@@ -158,15 +246,35 @@ function appendArtPiecesOrSubcategories(category, artArray) {
 function showSubCategories(subCategory, placeToAppend) {
   const clnSubCategory = subCategoryTemplate.cloneNode(true); //Template of subcategory
   const subCategoryName = clnSubCategory.querySelector(".subCategoryName")
-  subCategoryName.textContent = subCategory.post_title.toLowerCase()
 
+  let newClassName;
+  if (subCategory.post_title) {
+    newClassName = subCategory.post_title.toLowerCase();
+  } else if (subCategory.title.rendered) {
+    newClassName = subCategory.title.rendered.toLowerCase();
+  }
+  subCategoryName.textContent = newClassName;
+  const artSubcategory = clnSubCategory.querySelector(".artSubcategory");
+console.log(artSubcategory);
+  if (subCategory.art_piece_id) {
+    const convertedArtArray = Object.keys(subCategory.art_piece_id).map(i => subCategory.art_piece_id[i])
+    // appendArtPiecesOrSubcategories(subCategory, convertedArtArray)
+    convertedArtArray.forEach(art => {
+      console.log(art);
+      // console.log(subcategoryRt);
+      showArtPieceList(art, artSubcategory)
+    })
+
+  }
+
+
+
+  // showArtPieceList(piece, artSubcategory)
   const subcategory = clnSubCategory.querySelector(".subcategory");
-  subcategory.classList.add(subCategory.post_title.toLowerCase())
+  subcategory.classList.add(newClassName)
   //subCategory.art_piece_id is and object with objects inside, so i converted it to array
-  const convertedArtArray = Object.keys(subCategory.art_piece_id).map(i => subCategory.art_piece_id[i])
-  appendArtPiecesOrSubcategories(subCategory, convertedArtArray) //Function that will create and append open-folder container with art, but will not be displayed
-  openTheRightFolder(subcategory, subCategoryName.textContent) //Function that will display the correct open folder according on the subcategory clicked
-
+  //Function that will create and append open-folder container with art, but will not be displayed
+  // openTheRightFolder(subcategory, subCategoryName.textContent) //Function that will display the correct open folder according on the subcategory clicked
   placeToAppend.appendChild(clnSubCategory)
 }
 
@@ -174,9 +282,11 @@ function showSubCategories(subCategory, placeToAppend) {
 
 // Appends all the art pieces to the art-pieces container in the open-folder-container;
 function showArtPieceList(piece, placeToAppendTo) {
+  // console.log(piece);
   let artPieceCln = artPieceTemplate.cloneNode(true);
   artPieceCln.querySelector(".art-piece-name").textContent = piece.post_title.toLowerCase()
   artPieceCln.querySelector(".art-piece-large-icon").src = piece.featured_image.guid
+  // artPieces.appendChild(artPieceCln)
   placeToAppendTo.appendChild(artPieceCln) // Place to append is element with art-pieces class in the open-folder-container template;
 }
 
@@ -192,13 +302,14 @@ function getCategories(category, placeToAppend, namefontSize, namefontWeight, na
   categoryName.style.color = namefontColor;
   let imagesInsideFolderIcon = clnMenuFolder.querySelector(".images-inside-folder-icon");
   let categoryFolder = clnMenuFolder.querySelector(".category-folder");
+  // categoryFolder.classList.add("subcategory")
   // artCategories.appendChild(clnMenuFolder);
   placeToAppend.appendChild(clnMenuFolder);
 
   if (category.art_category_id.length > 0) { //If the category does NOT have subcategories
     addImgaesToFolderIcon(category, imagesInsideFolderIcon) //Add image to the folder icon
   }
-  openTheRightFolder(categoryFolder, categoryName.textContent) //When a folder is clicked it would remove display none property from the right element
+  // openTheRightFolder(categoryFolder, categoryName.textContent) //When a folder is clicked it would remove display none property from the right element
 }
 
 
@@ -212,17 +323,19 @@ function openTheRightFolder(folderToClick) {
     const folders = document.querySelectorAll(".artPieces") //selects all the open-folder-container that are already appended to the body but are not displayed
     const subcategories = document.querySelectorAll(".subcategory")
     subcategories.forEach(sub => {
-      sub.classList.remove("d-none")
+      sub.classList.add("d-none")
     })
+
     const folderName = folderToClick.querySelector(".name").textContent.toLowerCase().split(' ').join('')
     zIndex++; //this will increase so the folder would be on top
     folders.forEach(folder => { //goes through all open-folder-container h2
       folder.classList.add("d-none")
-      console.log(folder);
+      // console.log(folder);
       if (folder.classList.contains(`${folderName}`)) { //checks if the name of the h2, is the same as the name of the clicked folder
 
         openFolderContainer.classList.remove("d-none");
         folder.classList.remove("d-none")
+        // folder.querySelector(".artPieces").classList.remove("d-none");
         folder.style.zIndex = zIndex;
       }
     })
